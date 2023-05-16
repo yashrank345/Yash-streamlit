6c9bbd09732873ea0be7adbe414daf45ee175372
Lenovo <you@example.com>
2023-05-16 12:30:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def main():
               [Kaggle](https://www.kaggle.com/avikumart) 
               """)a,b,c = st.columns([0.2,0.6,0.2])
 with b:
-  st.image("banner-picture.jpeg", use_column_width=True)
+  st.image("banner-picture.jpeg", use_column_width=True) 
 
 
 # description about the project and code files            
